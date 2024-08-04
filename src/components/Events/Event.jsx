3fd5793fd86a0eb53bbad90b221c288e40d9ca15
Vidip <vidip2025@gmail.com>
2024-08-04 10:50:59 +0530

    Initial Commit
    Initialized the project

---
+++ src/components/Events/Event.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../Header/Header'
+import './Event.css';
+
+const Event = () => {
+  return (
+    <div>
+      <Header/>
+      <h1 className='body'>Events</h1>
+    </div>
+  )
+}
+
+export default Event
