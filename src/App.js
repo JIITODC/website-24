8cc74ae0224d7e080d3b7dc8fdcb7cfad457dab6
Tushar Singhal <tsinghal666@gmail.com>
2024-08-08 22:09:36 +0530

    Edited Join Page And Created Resources Page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import Home from "./components/Home/Home";
 import Event from "./components/Events/Event";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Resource from "./components/Resources/Resource";
 import Join from "./components/Join/join";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -13,7 +12,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/events" element={<Event />} />
-          <Route path="/resources" element={<Resource />} />
           <Route path="/join" element={<Join />} />
         </Routes>
       </BrowserRouter>
